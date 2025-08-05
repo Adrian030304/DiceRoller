82742b3a06c58052aff07abb838ad00353a8fab7
Adrian030304 <adrianromanenco1@gmail.com>
2025-08-05 17:33:30 +0300

    fix on choice validation loop

--- main.py
+++ main.py
@@ -7,17 +7,26 @@ import random
 print("Welcome to dice roller. You can throw roll dices typing (NdX).")
 print("N- number of throws.")
 print("X- number of dice sides.")
-try:
-    dice_roll_choice = input("Enter dice roll in the format NdX (e.g. 3d6): ")
-    dice_roll_choice = dice_roll_choice.lower().strip()
 
-    if  ('d' not in dice_roll_choice
-        or not dice_roll_choice.split('d')[1].isdigit()
-        or len(dice_roll_choice.split('d')) != 2):
-        raise ValueError(f"This is not respecting the format (NdX) -> {dice_roll_choice}")
-    
-except ValueError as err:
-    print(f"Warning: {err}.")
+while True:
+    try:
+        dice_roll_choice = input("Enter dice roll in the format NdX (e.g. 3d6): ")
+        dice_roll_choice = dice_roll_choice.lower().strip()
+
+        if  ('d' not in dice_roll_choice
+            or len(dice_roll_choice.split('d')) != 2
+            or not dice_roll_choice.split('d')[0].isdigit()
+            or not dice_roll_choice.split('d')[1].isdigit()
+            ):
+            raise ValueError(f"This is not respecting the format (NdX) -> {dice_roll_choice}")
+        
+    except ValueError as err:
+        print(f"Warning: {err}.")
+        continue
+    break
+
+print(f"dice_roll_choice {dice_roll_choice}")
+rolls_amount, dice_sides = dice_roll_choice.split('d')
 
 import sys
 sys.exit()
